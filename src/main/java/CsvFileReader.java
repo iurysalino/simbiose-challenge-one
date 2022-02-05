ec9d4eebdf19814148f13b69c8b58274325d46f1
iurysalino <iurysalino@gmail.com>
2022-02-05 03:29:11 -0300

    Implement Vector Schemma Root and Populate a Identical New Document

--- src/main/java/CsvFileReader.java
+++ src/main/java/CsvFileReader.java
@@ -1,7 +1,13 @@
 import java.io.IOException;
+import java.util.List;
+
+import org.apache.arrow.vector.VectorSchemaRoot;
 
 public class CsvFileReader {
   public static void main(String[] args) throws IOException {
-    new FileManipulation().csvFileReader(args[0]);
+    FileManipulation fm = new FileManipulation();
+    List lista = fm.csvFileReader(args[0]);
+    VectorSchemaRoot v = fm.vectorSchemaRootPopulate(lista);
+    fm.writeInCsv(v, args[1]);
   }
 }
--- src/main/java/FileManipulation.java
+++ src/main/java/FileManipulation.java
@@ -1,25 +1,89 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.arrow.memory.RootAllocator;
+import org.apache.arrow.vector.FieldVector;
+import org.apache.arrow.vector.VarCharVector;
+import org.apache.arrow.vector.VectorSchemaRoot;
 
 public class FileManipulation {
-  private String row;
 
-  public FileManipulation() {
+  public static final RootAllocator allocator = new RootAllocator(Long.MAX_VALUE);
+
+  public List csvFileReader(String path) throws FileNotFoundException {
+    String cell;
+    final VarCharVector vectorName = new VarCharVector("name", allocator);
+    final VarCharVector vectorTeam = new VarCharVector("team", allocator);
+    final VarCharVector vectorPosition = new VarCharVector("position", allocator);
+    final VarCharVector vectorHeight = new VarCharVector("height", allocator);
+    final VarCharVector vectorWeight = new VarCharVector("weight", allocator);
+    final VarCharVector vectorAge = new VarCharVector("age", allocator);
+
+    try {
+      BufferedReader csvReader = new BufferedReader(new FileReader(path));
+      int indice = 0;
+      while ((cell = csvReader.readLine()) != null) {
+        String[] data = cell.split(",");
+        vectorName.setSafe(indice, data[0].getBytes());
+        vectorTeam.setSafe(indice, data[1].getBytes());
+        vectorPosition.setSafe(indice, data[2].getBytes());
+        vectorHeight.setSafe(indice, data[3].getBytes());
+        vectorWeight.setSafe(indice, data[4].getBytes());
+        vectorAge.setSafe(indice, data[5].getBytes());
+        indice++;
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return Arrays.<FieldVector>asList(vectorName, vectorTeam, vectorPosition, vectorHeight, vectorWeight, vectorAge);
   }
 
-  public void csvFileReader(String path) {
-    try(BufferedReader csvReader = new BufferedReader(new FileReader(path))) {
-      while ((row = csvReader.readLine()) != null) {
-        String[] data = row.split(",");
-        for (String index : data) {
-          System.out.printf("%-10s", index);
-        }
-        System.out.println();
+  public VectorSchemaRoot vectorSchemaRootPopulate(List listsVetoes) {
+    VectorSchemaRoot vectorSchemaRoot = new VectorSchemaRoot(listsVetoes);
+    vectorSchemaRoot.setRowCount(1035);
+    return vectorSchemaRoot;
+  }
+
+  public void cellSeparator(FileWriter csvWriter) throws IOException {
+    csvWriter.append(",");
+  }
+
+  public void newLine(FileWriter csvWriter) throws IOException {
+    csvWriter.append("\n");
+  }
+
+  public void writeInCsv(VectorSchemaRoot vectorSchemaRoot, String path) {
+    VarCharVector vectorNameConsumer = (VarCharVector) vectorSchemaRoot.getVector(0);
+    VarCharVector vectorTeamConsumer = (VarCharVector) vectorSchemaRoot.getVector(1);
+    VarCharVector vectorPositionConsumer = (VarCharVector) vectorSchemaRoot.getVector(2);
+    VarCharVector vectorHeightConsumer = (VarCharVector) vectorSchemaRoot.getVector(3);
+    VarCharVector vectorWeightConsumer = (VarCharVector) vectorSchemaRoot.getVector(4);
+    VarCharVector vectorAgeConsumer = (VarCharVector) vectorSchemaRoot.getVector(5);
+
+    int count = 0;
+    try {
+      FileWriter csvWriter = new FileWriter(path);
+      while (count <= 1035) {
+        csvWriter.append(vectorNameConsumer.getObject(count).toString());
+        cellSeparator(csvWriter);
+        csvWriter.append(vectorTeamConsumer.getObject(count).toString());
+        cellSeparator(csvWriter);
+        csvWriter.append(vectorPositionConsumer.getObject(count).toString());
+        cellSeparator(csvWriter);
+        csvWriter.append(vectorHeightConsumer.getObject(count).toString());
+        cellSeparator(csvWriter);
+        csvWriter.append(vectorWeightConsumer.getObject(count).toString());
+        cellSeparator(csvWriter);
+        csvWriter.append(vectorAgeConsumer.getObject(count).toString());
+        cellSeparator(csvWriter);
+        newLine(csvWriter);
+        count++;
       }
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
     } catch (IOException e) {
       e.printStackTrace();
     }
